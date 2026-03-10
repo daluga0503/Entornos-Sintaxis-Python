de96df267de0cc9f76c8f6588758f6860fd26d41
daluga0503 <daniel.luquegallardo@gmail.com>
2026-03-10 20:43:06 +0100

    test ejercicio 1 realizado

--- Unidad 4/Proyecto-Testing-2/test/test_ejercicio01.py
+++ Unidad 4/Proyecto-Testing-2/test/test_ejercicio01.py
@@ -1,15 +1,26 @@
-from src.ejercicio01 import normaliza_nombre
+from ejercicio01 import normaliza_nombre
 import pytest
 
 @pytest.mark.parametrize("nombre, esperado", [
     ('DaNIeL LUga', 'Daniel Luga'),
-    ('Miguel ÁnGel', 'Miguel Ángel'),
-    ('Rosa-María', 'Rosa María'),
-    ('Antonio Manuel..', 'Antonio Manuel')
-    ('', '')
+    ('Miguel ÁnGel ', 'Miguel Ángel'),
+    (' rosa maría', 'Rosa María'),
+    ('Antonio  MANUEL', 'Antonio Manuel'),
 ])
+def test_normaliza_nombres_mayusculas_minusculas_espacios(nombre, esperado):
+    assert normaliza_nombre(nombre) == esperado
 
-def test_normaliza_nombres_mayusculas_minusculas(valor, esperado):
-    assert normaliza_nombre(valor) == esperado
+def test_normaliza_nombres_guines_entre_palabras():
+    assert normaliza_nombre('Ana-María Quintero') == 'Ana-María Quintero'
 
-def test_normaliza_nombre_(): 
+
+@pytest.mark.parametrize("valor", [
+    10,
+    10.0,
+    True,
+    None
+])
+def test_normalize_nombres_entrada_diferente_str(valor):
+    with pytest.raises(TypeError):
+        normaliza_nombre(valor)
+
